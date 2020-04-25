b0edfb626f573269a0581b8417ab121e2516bd1b
jhonny <anjandudda@gmail.com>
2020-04-25 20:05:07 +0530

    Components Part 1Components Part 1
    feat: components part 1

--- confusion/my-app/src/App.js
+++ confusion/my-app/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents';
 
 class App extends Component {
   render() {
+
     return (
+
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu/>
       </div>
     );
   }
